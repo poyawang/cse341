039234e08d609f0d9c247c84b099372aae63cae6
poyawang <poyawang12@gmail.com>
2022-02-03 00:00:44 -0700

    test heroku

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
 require('dotenv').config();
 
 const errorController = require('./controllers/error');
