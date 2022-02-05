82a5a122481487f419c238a616b2cb0d6c163001
poyawang <poyawang12@gmail.com>
2022-02-05 12:40:18 -0700

    session update

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 require('dotenv').config();
+
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI =
+`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.umzlh.mongodb.net/shop?`
+
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -18,9 +29,20 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
 
 app.use((req, res, next) => {
-  User.findById('61fb4cd2040ad22266810471')
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
       next();
@@ -35,16 +57,13 @@ app.use(authRoutes);
 app.use(errorController.get404);
 const port = process.env.PORT || 3000;
 mongoose
-  .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.umzlh.mongodb.net/shop?retryWrites=true&w=majority`
-    ,{useNewUrlParser: true, useUnifiedTopology:true}
-  )
+  .connect(MONGODB_URI)
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
         const user = new User({
-          name: 'Anna',
-          email: 'Anna@test.com',
+          name: 'Max',
+          email: 'max@test.com',
           cart: {
             items: []
           }
